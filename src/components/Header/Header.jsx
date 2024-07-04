9952f29a24876c00b90805c582112d316e2f7a7f
theoxav <theoxxav@outlook.fr>
2024-07-04 23:40:13 +0200

    Add responsive menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,35 @@
 import styles from "./Header.module.scss";
 import cookchef from "../../assets/images/cookchef.png";
+import { useState } from "react";
+import HeaderMenu from "../HeaderMenu/HeaderMenu";
 
-const Header = () => {
+function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
-    <header className={`${styles.header} d-flex flex-row align-items-center `}>
-      <i className="fa-solid fa-bars mr-15"></i>
+    <header className={`${styles.header} d-flex flex-row align-items-center`}>
       <div className="flex-fill">
         <img src={cookchef} alt="logo cookchef" />
       </div>
-      <ul>
+      <ul className={styles.headerList}>
         <button className="mr-5 btn btn-reverse-primary">
-          <i className="fa-solid fa-basket-shopping mr-5"></i>
-          <span>panier</span>
+          <i className="fa-solid fa-heart mr-5"></i>
+          <span>Wishlist</span>
         </button>
-        <button className="btn btn-primary">connexion</button>
+        <button className="btn btn-primary">Connexion</button>
       </ul>
+      <i
+        onClick={() => setShowMenu(true)}
+        className={`fa-solid fa-bars ${styles.headerXs}`}
+      ></i>
+      {showMenu && (
+        <>
+          <div onClick={() => setShowMenu(false)} className="calc"></div>
+          <HeaderMenu />
+        </>
+      )}
     </header>
   );
-};
+}
 
 export default Header;
--- src/components/Recipe/Recipe.jsx
+++ src/components/Recipe/Recipe.jsx
@@ -1,23 +1,26 @@
-import styles from "./Recipe.module.scss";
 import { useState } from "react";
-const Recipe = ({ title, image }) => {
+import styles from "./Recipe.module.scss";
+
+function Recipe({ title, image }) {
   const [liked, setLiked] = useState(false);
 
-  const handleClick = () => setLiked(!liked);
+  function handleClick() {
+    setLiked(!liked);
+  }
 
   return (
     <div onClick={handleClick} className={styles.recipe}>
       <div className={styles.imageContainer}>
-        <img src={image} alt="recipe" />
+        <img src={image} alt={title} />
       </div>
       <div
-        className={`${styles.recipeTitle} d-flex flex-row justify-content-center align-items-center`}
+        className={`${styles.recipeTitle} d-flex flex-column justify-content-center align-items-center`}
       >
         <h3 className="mb-10">{title}</h3>
         <i className={`fa-solid fa-heart ${liked ? "text-primary" : ""}`}></i>
       </div>
     </div>
   );
-};
+}
 
 export default Recipe;
