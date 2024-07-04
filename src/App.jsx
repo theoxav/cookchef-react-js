80cced93b1465aedf063e6d4a1a45579249a627c
theoxav <theoxxav@outlook.fr>
2024-07-04 19:54:50 +0200

    Init Header and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import styles from "./App.module.scss";
+import Content from "./components/Content/Content";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+
 const App = () => {
-  return <h1>Cookchef</h1>;
+  return (
+    <div className={`d-flex flex-column ${styles.appContainer}`}>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Content/Content.jsx
@@ -0,0 +1,5 @@
+const Content = () => {
+  return <h1 className="flex-fill">Content</h1>;
+};
+
+export default Content;
