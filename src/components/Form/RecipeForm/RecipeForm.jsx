6e19e7e147ede730946b0e085421019617651e90
theoxav <theoxxav@outlook.fr>
2024-07-05 11:56:29 +0200

    update form for add a recipe

--- src/components/Form/RecipeForm/RecipeForm.jsx
+++ src/components/Form/RecipeForm/RecipeForm.jsx
@@ -1,20 +1,88 @@
+import { useContext } from "react";
 import styles from "./RecipeForm.module.scss";
+import * as yup from "yup";
+import { ApiContext } from "../../../context/ApiContext";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 const RecipeForm = () => {
+  const BASE_URL = useContext(ApiContext);
+
+  const defaultValues = {
+    title: "",
+    image: "",
+  };
+
+  const recipeSchema = yup.object({
+    title: yup
+      .string()
+      .required("Le titre est obligatoire")
+      .min(10, "Le titre doit contenir au moins 10 caractères")
+      .max("30", "Le titre ne doit pas contenir plus de 30 caractères"),
+    image: yup
+      .string()
+      .required("L'image est obligatoire")
+      .url("Le lien est invalide"),
+  });
+
+  const {
+    formState: { errors, isSubmitting },
+    register,
+    handleSubmit,
+    reset,
+    setError,
+    clearErrors,
+  } = useForm({
+    defaultValues,
+    resolver: yupResolver(recipeSchema),
+  });
+
+  async function submit(values) {
+    try {
+      clearErrors();
+      const response = await fetch(BASE_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+      });
+      if (response.ok) {
+        reset(defaultValues);
+      } else {
+        setError("generic", {
+          type: "generic",
+          message: "Une erreur est survenue",
+        });
+      }
+    } catch (e) {
+      setError("generic", {
+        type: "generic",
+        message: "Une erreur est survenue",
+      });
+    }
+  }
+
   return (
-    <form className={`d-flex flex-column card p-20 ${styles.recipeForm}`}>
+    <form
+      onSubmit={handleSubmit(submit)}
+      className={`d-flex flex-column card p-20 ${styles.recipeForm}`}
+    >
       <h2 className="mb-20">Ajouter une recette</h2>
       <div className="d-flex flex-column mb-20">
         <label>Titre de la recette</label>
-        <input type="text" />
-        <p className="form-error">Test erreur</p>
+        <input {...register("title")} type="text" />
+        {errors.title && <p className="form-error">{errors.title.message}</p>}
       </div>
       <div className="d-flex flex-column mb-20">
         <label>Image pour la recette</label>
-        <input type="text" />
-        <p className="form-error">Test erreur</p>
+        <input {...register("image")} type="text" />
+        {errors.image && <p className="form-error">{errors.image.message}</p>}
       </div>
-      <button className="btn btn-primary">Sauvegarder</button>
+      {errors.generic && <p className="form-error">{errors.generic.message}</p>}
+      <button disabled={isSubmitting} className="btn btn-primary">
+        Sauvegarder
+      </button>
     </form>
   );
 };
